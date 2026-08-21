0e1f87984f4c76ffc3d9e4e47c249679a333f36f
luizamoll <mluizamoll@gmail.com>
2026-08-21 06:31:34 -0300

    test: adiciona teste inicial de contexto

---
+++ backend/src/test/java/br/com/imobcontrol/ImobControlApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.imobcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
