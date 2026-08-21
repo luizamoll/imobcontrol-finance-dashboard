21d9539fc4030c8c00f1a8b24244387c73039528
luizamoll <mluizamoll@gmail.com>
2026-08-21 06:31:14 -0300

    feat: adiciona aplicação principal do backend

---
+++ backend/src/main/java/br/com/imobcontrol/ImobControlApplication.java
@@ -0,0 +1,12 @@
+package br.com.imobcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImobControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImobControlApplication.class, args);
+    }
+}
