5ccb30e7adec38d6df4d0730c98e37684f24d78b
luizamoll <mluizamoll@gmail.com>
2026-09-16 14:56:16 -0300

    Executa limpeza única antes de iniciar a interface

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,6 @@
 import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
+import "./lib/empty-data-migration";
 import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
