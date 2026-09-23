82336002a48ce88471ffc30d97281a5c77791a5f
luizamoll <mluizamoll@gmail.com>
2026-09-23 18:11:56 -0300

    Adiciona contratos e auditoria do cadastro de clientes

---
+++ backend/src/main/java/br/com/imobcontrol/cliente/AuditoriaOperacionalRepository.java
@@ -0,0 +1,7 @@
+package br.com.imobcontrol.cliente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuditoriaOperacionalRepository
+        extends JpaRepository<AuditoriaOperacional, Long> {
+}
